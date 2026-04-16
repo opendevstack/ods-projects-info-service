b16f9023fd71c59e84ff79393c8362701cbc92b1
jordivx <jordivilaxicola@gmail.com>
2026-04-16 13:00:07 +0200

    Use obo flow to obtain graph token when using its api (#13)

--- src/main/java/org/opendevstack/projects_info_service/server/controllers/AzureGroupsApiController.java
+++ src/main/java/org/opendevstack/projects_info_service/server/controllers/AzureGroupsApiController.java
@@ -4,7 +4,9 @@ import org.opendevstack.projects_info_service.server.api.AzureGroupsApi;
 import org.opendevstack.projects_info_service.server.client.AzureGraphClient;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.opendevstack.projects_info_service.server.exception.GraphTokenException;
 import org.opendevstack.projects_info_service.server.facade.AuthenticationFacade;
+import org.opendevstack.projects_info_service.server.service.GraphTokenService;
 import org.opendevstack.projects_info_service.server.service.MocksService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,13 +25,14 @@ public class AzureGroupsApiController implements AzureGroupsApi {
     private AuthenticationFacade authenticationFacade;
     private final AzureGraphClient azureGraphClient;
     private final MocksService mocksService;
+    private final GraphTokenService graphTokenService;
 
     @Override
     public ResponseEntity<List<String>> getAzureGroups() {
         var accessToken = authenticationFacade.getAccessToken();
-
-        var userEmail = azureGraphClient.getUserEmail(accessToken);
-        var userGroups = azureGraphClient.getUserGroups(accessToken);
+        var graphToken = graphTokenService.getGraphToken(accessToken);
+        var userEmail = azureGraphClient.getUserEmail(graphToken);
+        var userGroups = azureGraphClient.getUserGroups(graphToken);
         var mockGroups = mocksService.getUserGroups(userEmail);
 
         var allGroups = Stream.concat(userGroups.stream(), mockGroups.stream())
---
+++ src/main/java/org/opendevstack/projects_info_service/server/exception/GraphTokenException.java
@@ -0,0 +1,7 @@
+package org.opendevstack.projects_info_service.server.exception;
+
+public class GraphTokenException extends RuntimeException {
+    public GraphTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
